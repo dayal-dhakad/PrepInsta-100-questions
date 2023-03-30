93577af6924899fb53cbbcc4dd68e2856a902813
Deendayal Dhakad <dayal.dhakad.125@gmail.com>
2023-03-31 01:45:47 +0530

    sol2

---
+++ sol1.cpp
@@ -0,0 +1,26 @@
+// // https://prepinsta.com/top-100-codes/
+// Positive or Negative number
+
+#include<iostream>
+using namespace std;
+ 
+ 
+int main(){
+ 
+ int num;
+ cin>>num;
+
+ if(num>0){
+    cout<<"It is a positive number";
+ }
+
+ else if(num==0){
+    cout<<"Zero";
+ }
+ else{
+    cout<<"It is a negative number";
+ }
+  
+  
+return 0;
+}
---
+++ sol2.cpp
@@ -0,0 +1,20 @@
+// Even or Odd number
+
+#include<iostream>
+using namespace std;
+ 
+ 
+int main(){
+ 
+  int num;
+  cin>>num;
+
+  if(num%2 == 0){
+    cout<<"It is a even number";
+  }
+  else{
+    cout<<"it is a odd number";
+  }
+  
+return 0;
+}
