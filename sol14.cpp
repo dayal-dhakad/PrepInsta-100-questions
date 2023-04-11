e260b3782c617045589ced04f4b35c85925ca870
Deendayal Dhakad <dayal.dhakad.125@gmail.com>
2023-04-11 12:23:44 +0530

    14

---
+++ sol14.cpp
@@ -0,0 +1,36 @@
+// armstrong number
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int number = 151;
+    int temp = number;
+    int num = number;
+    int count = 0;
+
+    while (number != 0)
+    {
+        number = number / 10;
+        count++;
+    }
+
+    int sum = 0;
+    while (temp != 0)
+    {
+        int bit = temp % 10;
+        sum += pow(bit, count);
+        temp = temp / 10;
+    }
+
+    if(sum==num){
+        cout<<num<<" is a armstrong number"<<endl;
+    }
+    else{
+        cout<<num<<" is not a armstrong number"<<endl;
+    }
+
+    return 0;
+}
