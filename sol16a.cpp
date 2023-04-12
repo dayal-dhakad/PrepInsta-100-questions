70fa86decd30407859ee23a09c70c8f072852971
Deendayal Dhakad <dayal.dhakad.125@gmail.com>
2023-04-13 02:16:20 +0530

    a

---
+++ sol16a.cpp
@@ -0,0 +1,30 @@
+//fibonacci series
+#include<iostream>
+using namespace std;
+ 
+ void fibonacci(int n){
+   static int n1 = 0;
+   static int n2 = 1;
+    int n3;
+
+    if(n>0){
+        n3 = n1+n2;
+        n1 = n2;
+        n2 = n3;
+        cout<<n3<<", ";
+           fibonacci(n-1);
+    }
+
+ 
+ }
+ 
+int main(){
+ 
+ int n = 10;
+
+ cout<<"0, 1, ";
+
+ fibonacci(n-2);
+  
+return 0;
+}
