0df1baeb02943a830f2a9715bb3ef738f09f7498
Deendayal Dhakad <dayal.dhakad.125@gmail.com>
2023-05-11 10:08:28 +0530

    29

--- sol29.cpp
+++ sol29.cpp
@@ -6,7 +6,18 @@ using namespace std;
  
 int main(){
  
-  
+ int num1 = 36, num2 = 60, HCF = 1;
+
+ for (int i = 1; i <=num1 || i<=num2 ; i++)
+ {
+    if (num1%i==0 && num2%i==0)
+    {
+        HCF = i;
+    }
+    
+ }
+ 
+  cout<<"HCF of the two numbers "<<num1<<" & "<<num2<<" is "<<HCF<<endl;
   
 return 0;
 }
