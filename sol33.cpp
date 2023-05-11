8f7dc168f1f2cc9fcc0f50b422854e28599833eb
Deendayal Dhakad <dayal.dhakad.125@gmail.com>
2023-05-11 13:16:23 +0530

    sol34

--- sol33.cpp
+++ sol33.cpp
@@ -1,2 +1,31 @@
 //Octal to Decimal Conversion
 
+#include<iostream>
+#include<math.h>
+using namespace std;
+ 
+int getDecimal(long long num){
+
+    int base  = 8;
+    int decimal = 0;
+    int i = 0;
+    
+
+    while(num!=0){
+        int digit = num%10;
+        decimal += digit* pow(base, i);
+        i++;
+        num = num/10;
+    }
+    return decimal;
+}
+ 
+int main(){
+ 
+ long long num = 462;
+
+ cout<< getDecimal(num);
+  
+  
+return 0;
+}
