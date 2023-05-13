eda2e8658a2dad330dbc3a5776948ddd3b64644d
Deendayal Dhakad <dayal.dhakad.125@gmail.com>
2023-05-13 19:03:01 +0530

    55

--- sol55.cpp
+++ sol55.cpp
@@ -1,29 +1,53 @@
-//Prime number using recusion
-#include<iostream>
+// Prime number using recusion
+#include <iostream>
 using namespace std;
 
+bool isPrime(int n, int i = 2)
+{
 
- 
-int main(){
- int num = 17;
+    if (n <= 2)
+    {
+        return (n == 2) ? true : false;
+    }
 
-//  int count = 0;
-//  for (int i = 2; i <=17/2; i++)
-//  {
-//     if (num%i==0)
-//     {
-//         count++;
-//     }
-  
-//  }
-//    if (count<=0)
-//     {
-//         cout<<"Prime number";
-//     }
-    
- 
- 
-  
-  
-return 0;
+    if (n % i == 0)
+    {
+        return false;
+    }
+    if (i * i > n)
+    {
+        return true;
+    }
+
+    return isPrime(n, i + 1);
+}
+
+int main()
+{
+    int num = 18;
+
+    if (isPrime(num))
+    {
+        cout << num << " is a prime number" << endl;
+    }
+    else
+    {
+        cout << "Not Prime" << endl;
+    }
+
+    //  int count = 0;
+    //  for (int i = 2; i <=17/2; i++)
+    //  {
+    //     if (num%i==0)
+    //     {
+    //         count++;
+    //     }
+
+    //  }
+    //    if (count<=0)
+    //     {
+    //         cout<<"Prime number";
+    //     }
+
+    return 0;
 }
