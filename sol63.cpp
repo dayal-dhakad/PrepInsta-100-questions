f8970be5fb25c598ba9068c924cf3e4d9c8529dc
Deendayal Dhakad <dayal.dhakad.125@gmail.com>
2023-05-15 10:10:36 +0530

    64

---
+++ sol63.cpp
@@ -0,0 +1,17 @@
+//Reverse array
+#include<iostream>
+using namespace std;
+ 
+ 
+int main(){
+ int arr[5] = { 4, 8, 2, 9, 3};
+
+ for (int\ i = 0; i < count; i++)
+ {
+    /* code */
+ }
+ 
+  
+  
+return 0;
+}
---
+++ sol65.cpp
@@ -0,0 +1,25 @@
+// sort an array in ascending and descending order
+#include<iostream>
+using namespace std;
+ 
+ 
+int main(){
+ int arr[6] = {4, 9, 1, 3, 2, 5};
+ int n = sizeof(arr)/sizeof(arr[0]);
+ for (int i = 0; i < n; i++)
+ {
+    for (int j = 0; j < n; j++)
+    {
+        if (arr[]<arr[j])
+        {
+            
+        }
+        
+    }
+    
+ }
+ 
+  
+  
+return 0;
+}
